dcee375068e69a3a0833f0e100add04871a91066
truongbeonb <40392548+truongbeonb@users.noreply.github.com>
2018-08-25 16:01:47 +0700

    đã cho đăng ký và đăng nhập vào home

---
+++ main.js
@@ -0,0 +1,18 @@
+
+    var modal = document.getElementById('id01');
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    var modal1 = document.getElementById('id02');
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal1.style.display = "none";
+        }
+    }
